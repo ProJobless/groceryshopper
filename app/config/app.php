ec0ecc7ac0e7d19fc8b61a9824c8652c36d5fb93
mutugi.gathuri <mutugi.gathuri@gmail.com>
2013-10-05 02:04:09 -0400

    generated the secret key

--- app/config/app.php
+++ app/config/app.php
@@ -65,7 +65,7 @@ return array(
 	|
 	*/
 
-	'key' => 'YourSecretKey!!!',
+	'key' => 'oHXiI7z1oWLlfHik7aAyz97nxHktfHBk',
 
 	/*
 	|--------------------------------------------------------------------------
