6a833242a116a2f3881b3c5da05ae5f1036686ec
Mutugi Gathuri <mutugi.gathuri@gmail.com>
2014-01-22 08:23:34 -0500

    Updated

---
+++ laravel/app/views/admin/pager.blade.php
@@ -0,0 +1,18 @@
+
+									<!-- pager and search -->
+									<div class="fg-toolbar ui-toolbar ui-widget-header ui-corner-bl ui-corner-br ui-helper-clearfix">
+										<div class="dataTables_filter" id="DataTables_Table_0_filter">
+											<label>Search: <input type="text" aria-controls="DataTables_Table_0" class=""></label>
+										</div>
+										<div class="dataTables_paginate fg-buttonset ui-buttonset fg-buttonset-multi ui-buttonset-multi paging_full_numbers" id="DataTables_Table_0_paginate">
+											<a tabindex="0" class="first ui-corner-tl ui-corner-bl fg-button ui-button ui-state-default ui-state-disabled" id="DataTables_Table_0_first">First</a>
+											<a tabindex="0" class="previous fg-button ui-button ui-state-default ui-state-disabled" id="DataTables_Table_0_previous">Previous</a>
+											<span><a tabindex="0" class="fg-button ui-button ui-state-default ui-state-disabled">1</a>
+											      <a tabindex="0" class="fg-button ui-button ui-state-default">2</a>
+											      <a tabindex="0" class="fg-button ui-button ui-state-default">3</a>
+											      <a tabindex="0" class="fg-button ui-button ui-state-default">4</a>
+											      <a tabindex="0" class="fg-button ui-button ui-state-default">5</a></span>
+											<a tabindex="0" class="next fg-button ui-button ui-state-default" id="DataTables_Table_0_next">Next</a>
+											<a tabindex="0" class="last ui-corner-tr ui-corner-br fg-button ui-button ui-state-default" id="DataTables_Table_0_last">Last</a>
+										</div>
+									</div><!-- /pager and search -->
