2e6f02d90e8faf7b0c77bab5844a840e1b59b393
Mutugi Gathuri <mutugi.gathuri@gmail.com>
2014-03-07 11:17:02 +0000

    Added language definations

---
+++ laravel/app/lang/en/admin/categories/messages.php
@@ -0,0 +1,23 @@
+<?php
+
+return array(
+  'already_exists' => 'Category already exists!',
+  'does_not_exist' => 'Category does not exist.',
+  'name_required'  => 'The name field is required',
+
+  'create' => array(
+    'error'   => 'Category was not created, please try again.',
+    'success' => 'Category created successfully.'
+  ),
+
+  'update' => array(
+    'error'   => 'Category was not updated, please try again',
+    'success' => 'Category updated successfully.'
+  ),
+
+  'delete' => array(
+    'error'   => 'There was an issue deleting the Category. Please try again.',
+    'success' => 'The Category was deleted successfully.'
+  )
+
+);
